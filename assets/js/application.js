c8e39f6e9e75957f15a037ef52f591c7075c05ec
Dave Russell <dave.kerr@gmail.com>
2017-06-18 16:15:17 +0100

    Added triangles

--- assets/js/application.js
+++ assets/js/application.js
@@ -18,15 +18,12 @@ $(function() {
 
 function loadImageToCanvas(e) {
     var img = $('<img />', {src: e.target.result});
-    console.log(e.target.result);
     $(img).on('load', function() {
-        console.log("img load")
         var canvas = $('#refCanvas')[0];
         var context = canvas.getContext('2d');
         context.imageSmoothingEnabled = true;
         context.imageSmoothingQuality = 'high';
 
-        // canvas.height = this.height;
         context.drawImage(this, 0, 0, canvas.width, canvas.height);
         ;
     });
@@ -36,7 +33,6 @@ function loadImageToCanvas(e) {
 }
 
 function addShapes(reps) {
-    // console.log("adding", x);
     var refCanvas = $('#refCanvas')[0];
     var refContext = refCanvas.getContext('2d');
 
@@ -44,42 +40,65 @@ function addShapes(reps) {
     var drawContext = drawCanvas.getContext('2d');
     var img = refContext.getImageData(0, 0, refCanvas.width, refCanvas.height);
 
-    for(i = 0; i < reps; i++) {
-        // pick a pixel
-        var x = Math.floor(Math.random() * refCanvas.width);
-        var y = Math.floor(Math.random() * refCanvas.height);
-        var key = Math.floor((y * refCanvas.width) + x) * 4;
-
-        // console.log(i, "/", reps);
-        var colour = colourHexFromKey(img, key);
-        drawTriangleAtPoint(x, y, colour, drawContext)
-    }
-
     if(window.initialHide) {
         $('#refCanvas').hide();
         $('#drawCanvas').show();
         window.initialHide = false;
+        blankCanvas(drawContext, drawCanvas);
     }
+
+    for(i = 0; i < reps; i++) {
+        var x = Math.floor(Math.random() * refCanvas.width);
+        var y = Math.floor(Math.random() * refCanvas.height);
+        var key = Math.floor((y * refCanvas.width) + x) * 4;
+
+        var colour = colourHexFromKey(img, key);
+        // if(Math.random() < 0.5) {
+        //     drawRectangleAtPoint(x, y, colour, drawContext);
+        // } else {
+            drawTriangleAtPoint(x, y, colour, drawContext);
+        // }
+    }
+
+    console.log("done")
+}
+
+function drawRectangleAtPoint(x, y, colour, context) {
+    var h = Math.floor(Math.random() * 6);
+    var w = Math.floor(Math.random() * 6);
+    context.fillStyle = colour;
+    context.fillRect(x-(w/2),y-(h/2),w,h);
 }
 
 function drawTriangleAtPoint(x, y, colour, context) {
+    var h = Math.floor(Math.random() * 6);
+    var w = Math.floor(Math.random() * 6);
+    var wobble = Math.round(Math.random() * 3);
     context.fillStyle = colour;
-    context.fillRect(x-1,y-1,3,3);
+    context.beginPath();
+    context.lineTo(x-wobble, y-h);
+    context.lineTo(x+w,y+wobble);
+    context.lineTo(x, y);
+    context.closePath();
+    context.fill();
 }
 
 function colourHexFromKey(img, key) {
     var r = img.data[key];
     var g = img.data[key+1];
     var b = img.data[key+2];
-    var a = Math.random();
+    var a = Math.random() * 0.75;
 
-    //var hex = "#" + r.toString(16) + g.toString(16) + b.toString(16) //+ a.toString(16);
     var hex = "rgba("+ r + "," + g + "," + b + "," + a + ")";
-    // console.log(hex);
     return hex;
 }
 
 function switchCanvas() {
     $('#refCanvas').toggle();
     $('#drawCanvas').toggle();
+}
+
+function blankCanvas(context, canvas) {
+    context.fillStyle = "#FFFFFF";
+    context.fillRect(0, 0, canvas.width, canvas.height);
 }
